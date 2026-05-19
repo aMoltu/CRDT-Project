1e797e5220450ea383071e0776590bdc1d989b53
Aleksander Emil Moltu <aleksaem@stud.ntnu.no>
2026-05-19 09:48:56 +0200

    add ci-cd pipeline

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
+  base: process.env.CI ? '/CRDT-Project/' : '/',
   plugins: [
     tailwindcss(),
     react(),
